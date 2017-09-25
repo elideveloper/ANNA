d0371a9dfa45f4944c289b1f06dbdcecd04003ae
Эльнар <e.nurullin@zonatelecom.ru>
2017-09-25 16:15:37 +0300

    main.cpp is refreshed

--- main.cpp
+++ main.cpp
@@ -19,29 +19,32 @@ double* randomlyDeviatedArray(double* arr, int numElem) {
 	return newArr;
 }
 
-
 /*
  * Add GA learning
  * #Automatic select of derivative for activation function
  * #High level setting of learning method
  * Dump and import neurons weights
- * M.b. return structure with error and number of iterations info.
+ * #M.b. return structure with error and number of iterations info.
+ * Different traning methods are implemented in private functions of ANN, then just call them in train()
  * */
 
 int main() {
 	std::srand(std::time(0));
     const double learningSpeed = 0.1;
-	const int numInp = 9;
-	const int numNeur = 3;
+	const int numInp = 16;
+	const int numNeur = 5;
 	const int numOutput = numInp;
-    double inputData[numInp] = { 0.5, 0.5, 0.5,
-							0.5, 0.5, 0.5, 
-							0.5, 0.5, 0.5 };
-	//double rightOut[numOutput] = inputData;
+    double inputData[numInp] = { 0.5, 0.5, 0.5, 0.5,
+								0.5, 0.5, 0.5, 0.5,
+								0.5, 0.5, 0.5, 0.5,
+								0.5, 0.5, 0.5, 0.5, };
+	//double rightOut[numOutput] = { 1 };
 
-    ANNA::ANN myAnn(numInp, numNeur, numOutput, ANNA::BP, ANNA::LOGISTIC_FUNCTION);
+    ANNA::ANN myAnn(numInp, numNeur, numOutput, ANNA::BP, ANNA::TANH_FUNCTION);
 	double* output = myAnn.computeOutput(inputData);
-	std::cout << "Initial:\n";
+	std::cout << "Initial input:\n";
+	printArr(inputData, numInp);
+	std::cout << "Initial output:\n";
 	printArr(output, numOutput);
 
     // prepare training dataset
@@ -50,22 +53,23 @@ int main() {
     double** trainOut = new double*[trainingSetSize];
     for (int i = 0; i < trainingSetSize; i++) {
         trainInp[i] = randomlyDeviatedArray(inputData, numInp);
-        trainOut[i] = new double[numInp];
-        for (int j = 0; j < numInp; j++) {
-            trainOut[i][j] = inputData[j];
+        trainOut[i] = new double[numOutput];
+        for (int j = 0; j < numOutput; j++) {
+            trainOut[i][j] = 0.5;
         }
     }
 
     // train
-    double err = myAnn.train(trainingSetSize, trainInp, trainOut, learningSpeed, 0.001, 10000);
-    std::cout << "Error avg: " << err << std::endl;
+    ANNA::TrainingResult trainingOutput = myAnn.train(trainingSetSize, trainInp, trainOut, learningSpeed, 0.01, trainingSetSize * 10);
+    std::cout << "Error avg: " << trainingOutput.avgError << std::endl;
+	std::cout << "Number of iterations: " << trainingOutput.numIterations << std::endl;
 
     // test
 	double* inpRand = randomlyDeviatedArray(inputData, numInp);
 	std::cout << "Random input:\n";
-	printArr(inpRand, numOutput);
+	printArr(inpRand, numInp);
 	double* outputRand = myAnn.computeOutput(inpRand);
-	std::cout << "Random output:\n";
+	std::cout << "Output:\n";
 	printArr(outputRand, numOutput);
 
 	system("pause");
